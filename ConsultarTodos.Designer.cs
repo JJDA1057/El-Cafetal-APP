e40c3450727d030d6fd9cd8ed965734bb7a6590b
salak301 <salak301@SALAK301-33>
2025-05-06 06:45:26 -0500

    Agregar archivos de proyecto.

---
+++ ConsultarTodos.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace El_Cafetal_APP
+{
+    partial class ConsultarTodos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewUsuarios = new DataGridView();
+            btnVolver = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsuarios).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewUsuarios
+            // 
+            dataGridViewUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewUsuarios.Location = new Point(31, 21);
+            dataGridViewUsuarios.Name = "dataGridViewUsuarios";
+            dataGridViewUsuarios.Size = new Size(757, 263);
+            dataGridViewUsuarios.TabIndex = 0;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Location = new Point(343, 333);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(133, 43);
+            btnVolver.TabIndex = 1;
+            btnVolver.Text = "Volver";
+            btnVolver.UseVisualStyleBackColor = true;
+            btnVolver.Click += btnVolver_Click;
+            // 
+            // ConsultarTodos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnVolver);
+            Controls.Add(dataGridViewUsuarios);
+            Name = "ConsultarTodos";
+            Text = "ConsultarTodos";
+            Load += ConsultarTodos_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsuarios).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewUsuarios;
+        private Button btnVolver;
+    }
+}
